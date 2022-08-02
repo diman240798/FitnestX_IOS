f29d733c9736771d59a59c565a274e13e5aed1f0
Chaban Dmitriy <30317169+diman240798@users.noreply.github.com>
2022-08-02 12:00:53 +0300

    Initial Commit

---
+++ FitnestX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnestX
+//
+//  Created by firecode id2 on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
